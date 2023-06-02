ebaf9a7fa924aaa80b8aea4b76abcebaa2a2e2cb
sanyath007 <sanyath007@gmail.com>
2023-06-02 11:38:27 +0700

    Create new Stat, Advice and Register view components and add all to App with thier route pathes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import DefaultLayout from "./components/DefaultLayout"
-import Login from './views/Auth/Login';
 import Home from './views/Home';
 import RepairList from './views/Repair/List';
 import AddRepair from './views/Repair/Add';
+import Stat from './views/Stat';
+import Advice from './views/Advice';
+import Login from './views/Auth/Login';
+import Register from './views/Auth/Register';
 
 function App() {
     return (
@@ -12,9 +15,12 @@ function App() {
                 <Route index element={<Home />} />
                 <Route path="list" element={<RepairList />} />
                 <Route path="add" element={<AddRepair />} />
+                <Route path="stat" element={<Stat />} />
+                <Route path="advice" element={<Advice />} />
                 <Route path="*" element={<div>404 Not Found</div>} />
             </Route>
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
         </Routes>
     );
 }
