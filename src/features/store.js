547f7e80713dc0162ebc11c7206e83ed44b0b170
sanyath007 <sanyath007@gmail.com>
2023-06-03 08:05:28 +0700

    Create new authService with using RTK Query and add its to store

--- src/features/store.js
+++ src/features/store.js
@@ -1,8 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
+import { authApi } from "../services/auth/authService";
 
 export default configureStore({
     reducer: {
         auth: authReducer,
+        [authApi.reducerPath]: authApi.reducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware),
 });
