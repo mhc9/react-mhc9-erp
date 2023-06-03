0dc4a0c13033c7ebd2bf76c92bc3657a6283b9e7
sanyath007 <sanyath007@gmail.com>
2023-06-03 07:00:17 +0700

    Re-design Home component UI and install react-icons package

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,11 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { FaInfoCircle } from 'react-icons/fa'
 import api from '../../api'
 
 const Home = () => {
-    const fetchEquipments = async () => {
-        const res = await api.get('/api/equipments');
+    const [loggedInUser, setLoggedInUser] = useState(null);
 
-        console.log(res);
+    const fetchEquipments = async () => {
+        try {
+            const res = await api.get('/api/auth/me');
+            
+            setLoggedInUser(res.data)
+        } catch (error) {
+            
+        }
     }
 
     useEffect(() => {
@@ -15,7 +22,17 @@ const Home = () => {
     }, []);
 
     return (
-        <div>Welcome to IT Helpdesk System</div>
+        <div>
+            <div className="flex items-center gap-3 border border-green-900 bg-green-300 rounded-md p-4 text-green-800">
+                <FaInfoCircle size={'50px'} />
+                <div className="flex flex-col">
+                    <h1 className="font-bold text-2xl">Welcome to IT Helpdesk System</h1>
+                    <p className="">
+                        {loggedInUser && loggedInUser.name}
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
 
