8910075b23230af9386dc568ad49015b65d04d07
sanyath007 <sanyath007@gmail.com>
2023-06-07 16:59:22 +0700

    Create new assetSlice and add its to store

---
+++ src/features/asset/assetSlice.js
@@ -0,0 +1,49 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import api from '../../api';
+
+const initialState = {
+    assets: [],
+    pager: null,
+    loading: false,
+    success: false,
+    error: null
+};
+
+export const getAssets = createAsyncThunk("asset/getAssets", async (data, { rejectWithValue }) => {
+    try {
+        const res = await api.get(`/api/assets`);
+
+        return res.data;
+    } catch (error) {
+        rejectWithValue(error);
+    }
+});
+
+export const assetSlice = createSlice({
+    name: 'asset',
+    initialState,
+    reducers: {},
+    extraReducers: {
+        [getAssets.pending]: (state) => {
+            state.assets = [];
+            state.pager = null;
+            state.loading = true;
+            state.success = false;
+            state.error = null;
+        },
+        [getAssets.fulfilled]: (state, { payload }) => {
+            const { data, ...pager } = payload;
+
+            state.assets = data;
+            state.pager = pager;
+            state.loading = false
+            state.success = true;
+        },
+        [getAssets.rejected]: (state, { payload }) => {
+            state.loading = false;
+            state.error = payload;
+        },
+    }
+});
+
+export default assetSlice.reducer;
--- src/features/store.js
+++ src/features/store.js
@@ -1,6 +1,7 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
 import equipmentReducer from "./equipment/equipmentSlice";
+import asssetReducer from "./asset/assetSlice";
 import { authApi } from "../services/auth/authService";
 
 export default configureStore({
@@ -8,6 +9,7 @@ export default configureStore({
         auth: authReducer,
         [authApi.reducerPath]: authApi.reducer,
         equipment: equipmentReducer,
+        asset: asssetReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware),
 });
