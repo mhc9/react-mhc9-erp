ec55ab8bd7b0c86795219a98ed3ba6f37b7e9599
sanyath007 <sanyath007@gmail.com>
2023-06-07 19:35:25 +0700

    Add interceptors method of axios to api for checking if respose error of 401 then redirect to login page

--- src/api/index.js
+++ src/api/index.js
@@ -14,4 +14,14 @@ api.interceptors.request.use((config) => {
     return config;
 }, (error) => Promise.reject(error));
 
+api.interceptors.response.use((response) => {
+    return response;
+}, (error) => {
+    if (error.response.status === 401) {
+        return window.location.href = "/login";
+    } else {
+        return Promise.reject(error);
+    }
+})
+
 export default api;
