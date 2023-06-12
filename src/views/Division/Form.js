74a853119cfb3999f76f2e45bde88505654e2091
sanyath007 <sanyath007@gmail.com>
2023-06-12 15:07:18 +0700

    Add department_id field, its input and prop of divisionSchema and create departments and setDepartments states and getDepartments method to DivisionForm

--- src/views/Division/Form.js
+++ src/views/Division/Form.js
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Field, Form, Formik } from 'formik'
 import * as Yup from 'yup'
 import Loading from '../../components/Loading'
 import { store, update } from '../../features/division/divisionSlice'
+import api from '../../api';
 
 const divisionSchema = Yup.object().shape({
-    name: Yup.string().required()
+    name: Yup.string().required(),
+    department_id: Yup.string().required(),
 });
 
 const DivisionForm = ({ division, handleCancel }) => {
     const dispatch = useDispatch();
     const { loading } = useSelector(state => state.division);
+    const [departments, setDepartments] = useState([]);
+
+    useEffect(() => {
+        getDepartments();
+
+        return () => getDepartments();
+    }, [])
+
+    const getDepartments = async () => {
+        try {
+            const res = await api.get(`/api/divisions/form/init`);
+
+            setDepartments(res.data.departments);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     const handleSubmit = (values, props) => {
         if (division) {
@@ -31,6 +50,7 @@ const DivisionForm = ({ division, handleCancel }) => {
                 enableReinitialize
                 initialValues={{
                     name: division ? division.name : '',
+                    department_id: division ? division.department_id : '',
                     status: division ? (division.status === 1 ? true : false) : false,
                 }}
                 validationSchema={divisionSchema}
@@ -48,6 +68,14 @@ const DivisionForm = ({ division, handleCancel }) => {
                                     className="form-control w-4/12"
                                     placeholder="ชื่องาน"
                                 />
+                                <select name="department_id" className="form-control w-4/12">
+                                    <option>-- เลือกกลุ่มงาน --</option>
+                                    {departments && departments.map(dep => (
+                                        <option key={dep.id} value={dep.id}>
+                                            {dep.name}
+                                        </option>
+                                    ))}
+                                </select>
                                 <div className="flex items-center justify-center gap-2">
                                     <Field
                                         type="checkbox"
