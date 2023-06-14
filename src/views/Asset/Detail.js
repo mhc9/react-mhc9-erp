3b1fc80cf6cf4059a42e03b415976a349c83c727
sanyath007 <sanyath007@gmail.com>
2023-06-14 22:13:22 +0700

    Re-design AssetDetail by using useParams hook to get id param and render its in Breadcrumb.Item and page's title

--- src/views/Asset/Detail.js
+++ src/views/Asset/Detail.js
@@ -1,21 +1,24 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { Breadcrumb } from 'react-bootstrap'
 
 const AssetDetail = () => {
+    const { id } = useParams();
+
     return (
         <div className="content-wrapper">
             {/* breadcrumb */}
             <Breadcrumb>
                 <Breadcrumb.Item href="/">หน้าหลัก</Breadcrumb.Item>
                 <Breadcrumb.Item href="/">ข้อมูลพื้ฐาน</Breadcrumb.Item>
-                <Breadcrumb.Item active>รายละเอียดพัสดุ</Breadcrumb.Item>
+                <Breadcrumb.Item href="/asset">รายการพัสดุ</Breadcrumb.Item>
+                <Breadcrumb.Item active>{id}</Breadcrumb.Item>
             </Breadcrumb>
         
             <div className="content">
                 <div className="flex items-center justify-between mb-2">
-                    <h2 className="text-xl">รายละเอียดพัสดุ</h2>
-                    
+                    <h2 className="text-xl">รายละเอียดพัสดุ : {id}</h2>
+
                 </div>
             </div>
         </div>
