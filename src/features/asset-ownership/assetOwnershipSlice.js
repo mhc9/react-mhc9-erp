46c965f942ee5a4bc0f9b047aa3c7a9c59551025
sanyath007 <sanyath007@gmail.com>
2023-06-17 16:47:59 +0700

    Create new update and delete async action in assetOwnershipSlice

--- src/features/asset-ownership/assetOwnershipSlice.js
+++ src/features/asset-ownership/assetOwnershipSlice.js
@@ -29,6 +29,36 @@ export const getOwnershipsByAsset = createAsyncThunk("ownership/getOwnershipsByA
     }
 });
 
+export const store = createAsyncThunk("ownership/store", async (data, { rejectWithValue }) => {
+    try {
+        const res = await api.post(`/api/asset-ownerships`, data);
+
+        return res.data;
+    } catch (error) {
+        rejectWithValue(error)
+    }
+});
+
+export const update = createAsyncThunk("ownership/update", async ({ id, data }, { dispatch, rejectWithValue }) => {
+    try {
+        const res = await api.put(`/api/asset-ownerships/${id}`, data);
+
+        return res.data;
+    } catch (error) {
+        rejectWithValue(error)
+    }
+});
+
+export const destroy = createAsyncThunk("ownership/store", async ({ id }, { dispatch, rejectWithValue }) => {
+    try {
+        const res = await api.delete(`/api/asset-ownerships/${id}`);
+
+        return res.data;
+    } catch (error) {
+        rejectWithValue(error)
+    }
+});
+
 export const assetOwnershipSlice = createSlice({
     name: 'ownership',
     initialState,
@@ -64,6 +94,36 @@ export const assetOwnershipSlice = createSlice({
         },
         [getOwnershipsByAsset.rejected]: (state, { payload }) => {
             state.loading = false;
+            state.error = payload;
+        },
+        [store.pending]: (state) => {
+            state.success = false;
+            state.error = null;
+        },
+        [store.fulfilled]: (state, { payload }) => {
+            state.success = true;
+        },
+        [store.rejected]: (state, { payload }) => {
+            state.error = payload;
+        },
+        [update.pending]: (state) => {
+            state.success = false;
+            state.error = null;
+        },
+        [update.fulfilled]: (state, { payload }) => {
+            state.success = true;
+        },
+        [update.rejected]: (state, { payload }) => {
+            state.error = payload;
+        },
+        [destroy.pending]: (state) => {
+            state.success = false;
+            state.error = null;
+        },
+        [destroy.fulfilled]: (state, { payload }) => {
+            state.success = true;
+        },
+        [destroy.rejected]: (state, { payload }) => {
             state.error = payload;
         },
     }
