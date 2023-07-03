31ae4a8110e98c9760a6e2c0d3f0ae2ecdbd56b0
sanyath007 <sanyath007@gmail.com>
2023-07-03 12:04:15 +0700

    Mocify CardList of ModalItemList by splitting item's description to itself component and add its to CardList instead

--- src/components/Modals/ItemList/CardList.js
+++ src/components/Modals/ItemList/CardList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card, Col, Row } from 'react-bootstrap'
 import { currency } from '../../../utils'
+import ItemDesc from './ItemDesc'
 
 const CardList = ({ items, onHide, onSelect }) => {
     return (
@@ -8,14 +9,11 @@ const CardList = ({ items, onHide, onSelect }) => {
             {items && items.map((item, index) => (
                 <Col md={3} key={item.id}>
                     <Card>
-                        <Card.Img src={item.img_url} />
+                        <Card.Img src={item.img_url} className="w-[120px] h-auto self-center" />
                         <Card.Body>
-                            <p className="text-gray-500 text-sm">{item.category?.name}</p>
-                            <p className="mb-1 text-sm">{item.name}</p>
-                            <div className="border rounded-md mb-1 p-2 text-xs min-h-[100px]">
-                                <h3 className="font-bold underline">รายละเอียด</h3>
-                                <p className="text-gray-400 font-thin">{item.description}</p>
-                            </div>
+                            <p className="text-gray-500 text-xs">{item.category?.name}</p>
+                            <p className="text-xs min-h-[50px]">{item.name}</p>
+                            <ItemDesc item={item} />
                             <p className="font-bold text-lg">
                                 ราคา <span className="text-red-500">{currency.format(item.price)}</span> บาท
                             </p>
