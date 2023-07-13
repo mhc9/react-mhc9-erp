c398c3554c4d60c19fbab59de5dabbc24c015eb8
sanyath007 <sanyath007@gmail.com>
2023-07-13 15:19:43 +0700

    Create new handleInputChange and handleFilter methods and filters local state and add category input in FilteringInputs of ModalItemList

--- src/components/Modals/ItemList/FilteringInputs.js
+++ src/components/Modals/ItemList/FilteringInputs.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Col, FormGroup, Row } from 'react-bootstrap'
 
-const FilteringInputs = () => {
+const FilteringInputs = ({ initialFilters, onFilter, formData }) => {
+    const [filters, setFilters] = useState(initialFilters);
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+
+        setFilters({ ...filters, [name]: value })
+    };
+
+    const handleFilter = () => {
+        let queryStr = '';
+        for (const [key, val] of Object.entries(filters)) {
+            queryStr += `&${key}=${val}`;
+        }
+
+        onFilter(queryStr);
+    };
+
     return (
         <Row className="mb-3">
             <Col>
@@ -10,9 +27,17 @@ const FilteringInputs = () => {
                         <input
                             type="text"
                             name=""
-                            placeholder=""
+                            placeholder="ค้นหาด้วยชื่อ"
                             className="form-control"
                         />
+                    </FormGroup>
+                    <FormGroup>
+                        <select
+                            name="category"
+                            className="form-control"
+                        >
+                            <option value="">-- ทั้งหมด --</option>
+                        </select>
                     </FormGroup>
                     <FormGroup>
                         <button type="button" className="btn btn-outline-secondary">ตกลง</button>
