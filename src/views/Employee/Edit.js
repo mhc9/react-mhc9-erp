973c3c685e852d81eb79fa61688a892cfd07787a
sanyath007 <sanyath007@gmail.com>
2023-07-25 15:56:24 +0700

    Create new EditEmployee and add its route path to App

--- src/views/Employee/Edit.js
+++ src/views/Employee/Edit.js
@@ -3,7 +3,7 @@ import { Breadcrumb } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
 import EmployeeForm from './Form'
 
-const EmployeeDetail = () => {
+const EditEmployee = () => {
     const { id } = useParams();
 
     return (
@@ -13,12 +13,12 @@ const EmployeeDetail = () => {
                 <Breadcrumb.Item href="/">หน้าหลัก</Breadcrumb.Item>
                 <Breadcrumb.Item active>ข้อมูลพื้ฐาน</Breadcrumb.Item>
                 <Breadcrumb.Item href="/employee">บุคลากร</Breadcrumb.Item>
-                <Breadcrumb.Item active>รายละเอียดบุคลากร</Breadcrumb.Item>
+                <Breadcrumb.Item active>แก้ไขบุคลากร</Breadcrumb.Item>
                 <Breadcrumb.Item active>{id}</Breadcrumb.Item>
             </Breadcrumb>
         
             <div className="content">
-                <h2 className="text-xl">รายละเอียดบุคลากร ID: {id}</h2>
+                <h2 className="text-xl">แก้ไขบุคลากร ID: {id}</h2>
 
                 <div className="my-2 border p-4 rounded-md">
                     <EmployeeForm />
@@ -28,4 +28,4 @@ const EmployeeDetail = () => {
     )
 }
 
-    export default EmployeeDetail
+    export default EditEmployee
