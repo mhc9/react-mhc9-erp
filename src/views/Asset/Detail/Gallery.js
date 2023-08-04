bde4325c21c9c1c9f74bb4f6ba0b836a7925a397
sanyath007 <sanyath007@gmail.com>
2023-08-04 14:03:27 +0700

    Re-design Gallery by using Row and Col react-bootstrap component for rendering sub-image instead

--- src/views/Asset/Detail/Gallery.js
+++ src/views/Asset/Detail/Gallery.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import { Row, Col } from 'react-bootstrap'
 
 const Gallery = () => {
     return (
         <div className="flex flex-col">
             <h3 className="mb-2 font-bold">รูปพัสดุ</h3>
-            <div className="border w-full h-[150px] text-center rounded-md mb-2">
+            <div className="border w-full h-[220px] text-center rounded-md mb-2">
                 <img src='' alt='asset-thumbnail' />
             </div>
-            <div className="flex flex-row gap-1">
+            <Row className="max-md:hidden gx-2">
                 {[1,2,3,4].map((image, index) => (
-                    <div key={index} className="border rounded-md w-4/12 h-[80px]">image 1</div>
+                    <Col key={index} className="text-center mb-2" md={3}>
+                        <img src={''} alt={`image ${index+1}`} className="border rounded-md h-[80px]" />
+                    </Col>
                 ))}
-            </div>
+            </Row>
         </div>
     )
 }
