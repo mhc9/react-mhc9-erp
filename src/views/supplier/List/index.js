234b5892ebb850e9871f033ac7240d7d81036e45
sanyath007 <sanyath007@gmail.com>
2023-10-04 15:17:18 +0700

    Create new Pagination component and use its in SupplierList and reflactor SupplierList by calling dispatch getSuppliers action in useEffect hook on component did mount

--- src/views/supplier/List/index.js
+++ src/views/supplier/List/index.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { Breadcrumb } from 'react-bootstrap'
+import Pagination from '../../../components/Pagination'
+import { getSuppliers } from '../../../features/supplier/supplierSlice'
 
 const SupplierList = () => {
+    const dispatch = useDispatch();
+    const { suppliers, pager, isLoading } = useSelector(state => state.supplier);
+
+    useEffect(() => {
+        dispatch(getSuppliers({ url: `/api/suppliers/search` }))
+    }, []);
+
     return (
         <div className="content-wrapper">
             {/* breadcrumb */}
@@ -19,8 +29,28 @@ const SupplierList = () => {
                 </div>
 
                 <div>
-                    {/* // Table data */}
+                    <table className="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>ชื่อผู้จัดจำหน่าย</th>
+                                <th>ที่อยู่</th>
+                                <th>เจ้าของ</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {suppliers && suppliers.map((supplier, index) => (
+                                <tr>
+                                    <td>{pager && pager.from + index}</td>
+                                    <td>{supplier.name}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
+
+                <Pagination pager={pager} />
             </div>
         </div>
     )
