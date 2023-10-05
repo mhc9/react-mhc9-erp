8eeef808d8d92eb009f5f0d9884632bd199a0ca8
sanyath007 <sanyath007@gmail.com>
2023-10-05 09:23:53 +0700

    Add ToastContainer component to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import store from "./features/store";
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
+
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,6 +16,7 @@ root.render(
     <BrowserRouter>
       <Provider store={store}>
         <App />
+        <ToastContainer />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
