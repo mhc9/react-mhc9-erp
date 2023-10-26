509d2017e13e389558864a98e88641c0169c498d
sanyath007 <sanyath007@gmail.com>
2023-10-26 11:48:46 +0700

    Reflactor AssetFilteringInput by adding filter and clear buttons with handleFilter and handleClearInputs methods

--- src/components/Asset/FilteringInput.js
+++ src/components/Asset/FilteringInput.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Col, Row } from 'react-bootstrap'
+import { Col, Row, FormGroup } from 'react-bootstrap'
 import { generateQueryString } from '../../utils'
 import { useGetInitialFormDataQuery } from '../../services/asset/assetApi';
 import Loading from '../Loading';
@@ -18,76 +18,98 @@ const AssetFilteringInput = ({ initialFilters, onFilter }) => {
         const _filters = { ...filters, [name]: value };
 
         setFilters(_filters);
-        onFilter(generateQueryString(_filters));
+    };
+
+    const handleFilter = () => {
+        onFilter(generateQueryString(filters));
+    };
+
+    const handleClearInputs = () => {
+        setFilters(initialFilters);
+
+        onFilter(generateQueryString(initialFilters));
     };
 
     return (
-        <div className="flex flex-col gap-2 border rounded-md p-2 my-2">
-            <Row>
-                <Col md={4}>
-                    <input
-                        type="text"
-                        name="name"
-                        value={filters.name}
-                        onChange={handleInputChange}
-                        className="form-control text-sm font-thin"
-                        placeholder="ชื่อพัสดุ"
-                    />
-                </Col>
-                {/* <Col md={4}>
-                    <select
-                        name="category"
-                        value={filters.category}
-                        onChange={handleInputChange}
-                        className="form-control"
-                    >
-                        <option value="">-- ชนิดพัสดุ --</option>
-                        {categories && categories.map(category => (
-                            <option key={category.id} value={category.id}>
-                                {category.name}
-                            </option>
-                        ))}
-                    </select>
-                </Col> */}
-                <Col md={4}>
-                    {isLoading && <div className="form-control text-center"><Loading /></div>}
-                    {!isLoading && (
-                        <select
-                            name="group"
-                            value={filters.group}
+        <Row>
+            <Col>
+                <div className="flex flex-row gap-2 border rounded-md p-2 my-2">
+                    <FormGroup>
+                        <input
+                            type="text"
+                            name="name"
+                            value={filters.name}
                             onChange={handleInputChange}
                             className="form-control text-sm font-thin"
+                            placeholder="ชื่อพัสดุ"
+                        />
+                    </FormGroup>
+                    {/* <FormGroup>
+                        <select
+                            name="category"
+                            value={filters.category}
+                            onChange={handleInputChange}
+                            className="form-control"
                         >
-                            <option value="">-- กลุ่มพัสดุ --</option>
-                            {formData.groups && formData.groups.map(group => (
-                                <option key={group.id} value={group.id}>
-                                    {group.name}
+                            <option value="">-- ชนิดพัสดุ --</option>
+                            {categories && categories.map(category => (
+                                <option key={category.id} value={category.id}>
+                                    {category.name}
                                 </option>
                             ))}
                         </select>
-                    )}
-                </Col>
-                <Col md={4}>
-                    {isLoading && <div className="form-control text-center"><Loading /></div>}
-                    {!isLoading && (
-                        <select
-                            name="owner"
-                            value={filters.owner}
-                            onChange={handleInputChange}
-                            className="form-control text-sm font-thin"
-                        >
-                            <option value="">-- ผู้รับผิดชอบ --</option>
-                            {formData.employees && formData.employees.map(employee => (
-                                <option key={employee.id} value={employee.id}>
-                                    {`${employee.firstname} ${employee.lastname}`}
-                                </option>
-                            ))}
-                        </select>
-                    )}
-                </Col>
-                <Col></Col>
-            </Row>
-        </div>
+                    </FormGroup> */}
+                    <FormGroup>
+                        {isLoading && <div className="form-control text-center"><Loading /></div>}
+                        {!isLoading && (
+                            <select
+                                name="group"
+                                value={filters.group}
+                                onChange={handleInputChange}
+                                className="form-control text-sm font-thin"
+                            >
+                                <option value="">-- กลุ่มพัสดุ --</option>
+                                {formData.groups && formData.groups.map(group => (
+                                    <option key={group.id} value={group.id}>
+                                        {group.name}
+                                    </option>
+                                ))}
+                            </select>
+                        )}
+                    </FormGroup>
+                    <FormGroup>
+                        {isLoading && <div className="form-control text-center"><Loading /></div>}
+                        {!isLoading && (
+                            <select
+                                name="owner"
+                                value={filters.owner}
+                                onChange={handleInputChange}
+                                className="form-control text-sm font-thin"
+                            >
+                                <option value="">-- ผู้รับผิดชอบ --</option>
+                                {formData.employees && formData.employees.map(employee => (
+                                    <option key={employee.id} value={employee.id}>
+                                        {`${employee.firstname} ${employee.lastname}`}
+                                    </option>
+                                ))}
+                            </select>
+                        )}
+                    </FormGroup>
+                    <FormGroup>
+                        <button type="button" className="btn btn-outline-secondary text-sm" onClick={handleFilter}>
+                                ตกลง
+                            </button>
+                            <button
+                                type="button"
+                                className="btn btn-outline-danger text-sm ml-1"
+                                onClick={handleClearInputs}
+                            >
+                                เคลียร์
+                            </button>
+                    </FormGroup>
+                </div>
+            </Col>
+        </Row>
     )
 }
 
