8af987904ecfc3d41b013b26d61f52a732c0f787
sanyath007 <sanyath007@gmail.com>
2024-04-26 15:07:04 +0700

    reflactor(AddLoanRefund): add page head text

--- src/views/LoanRefund/Add.js
+++ src/views/LoanRefund/Add.js
@@ -9,10 +9,11 @@ const AddLoanRefund = () => {
                 <Breadcrumb.Item href="/">หน้าหลัก</Breadcrumb.Item>
                 <Breadcrumb.Item active>ยืมเงินราชการ</Breadcrumb.Item>
                 <Breadcrumb.Item href="/loan-refund">รายการหักล้างเงินยืม</Breadcrumb.Item>
-                <Breadcrumb.Item active>เพิ่มรายการ</Breadcrumb.Item>
+                <Breadcrumb.Item active>เพิ่มรายการหักล้างเงินยืม</Breadcrumb.Item>
             </Breadcrumb>
 
             <div className="content">
+                <h1 className="text-xl font-bold mb-1">เพิ่มรายการหักล้างเงินยืม</h1>
 
                 <LoanRefundForm />
 
