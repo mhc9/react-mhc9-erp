f0b7c454dafbc408e27d20aa5eecdcadcfdbe941
sanyath007 <sanyath007@gmail.com>
2024-07-05 15:13:43 +0700

    reflactor(Home): edit system name of welcome text

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -20,7 +20,7 @@ const Home = () => {
             <div className="flex items-center gap-3 border border-green-900 bg-green-300 rounded-md p-4 text-green-800">
                 <FaInfoCircle size={'50px'} />
                 <div className="flex flex-col">
-                    <h1 className="font-bold text-2xl">Welcome to IT Helpdesk System</h1>
+                    <h1 className="font-bold text-2xl">Welcome to MHC9 ERP System</h1>
                     <p className="">
                         {user && user.name}
                     </p>
