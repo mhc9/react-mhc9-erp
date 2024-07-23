74a58c931972c670e584705fe93e7df7f6e0524b
sanyath007 <sanyath007@gmail.com>
2024-07-23 17:11:23 +0700

    reflactor(Requisition/Detail/Consideration): create new handleSubmit method and approvalSchema const and approval prop

--- src/views/Requisition/Detail/Consideration.js
+++ src/views/Requisition/Detail/Consideration.js
@@ -1,30 +1,51 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { toast } from 'react-toastify'
 import { FaSearch } from 'react-icons/fa'
 import { DatePicker } from '@material-ui/pickers'
 import { Col, Row } from 'react-bootstrap'
 import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
 import moment from 'moment'
+import { consider } from '../../../features/slices/approval/approvalSlice'
 import ModalSupplierList from '../../../components/Modals/Supplier'
 
-const Consideration = ({ requisition }) => {
+const approvalSchema = Yup.object().shape({
+    consider_no: Yup.string().required(),
+    consider_date: Yup.string().required(),
+    notice_date: Yup.string().required(),
+    supplier_id: Yup.string().required(),
+    deliver_date: Yup.string().when('notice_date', {
+        is: (val) => val !== '',
+        then: () => Yup.string().required()
+    }),
+    deliver_days: Yup.string().min(1, "กำหนดส่งมอบภายใน (วัน) ต้องมากกว่า 0").required(),
+});
+
+const Consideration = ({ approval, requisition }) => {
+    const dispatch = useDispatch();
     const [showSupplierModal, setShowSupplierModal] = useState(false);
     const [selectedSupplier, setSelectedSupplier] = useState(null);
     const [selectedConsiderDate, setSelectedConsiderDate] = useState(moment());
     const [selectedNoticeDate, setSelectedNoticeDate] = useState(moment());
     const [selectedDeliverDate, setSelectedDeliverDate] = useState(moment());
 
+    const handleSubmit = (values, formik) => {
+        dispatch(consider({ id: requisition.id, data: values }))
+    };
+
     return (
         <Formik
             initialValues={{
-                consider_no: '',
-                consider_date: '',
-                notice_date: '',
-                supplier_id: '',
-                deliver_date: '',
-                deliver_days: '0',
+                consider_no: approval ? approval.consider_no : '',
+                consider_date: approval ? approval.consider_date : '',
+                notice_date: approval ? approval.notice_date : '',
+                supplier_id: approval ? approval.supplier_id : '',
+                deliver_date: approval ? approval.deliver_date : '',
+                deliver_days: approval ? approval.deliver_days : 0,
             }}
-            onSubmit={(values, formik) => console.log(values)}
+            validationSchema={approvalSchema}
+            onSubmit={handleSubmit}
         >
             {(formik) => {
                 return (
@@ -116,15 +137,28 @@ const Consideration = ({ requisition }) => {
                                         onChange={(date) => {
                                             setSelectedDeliverDate(date);
                                             formik.setFieldValue('deliver_date', date.format('YYYY-MM-DD'));
+
+                                            /** คำนวณวันกำหนดส่งมอบภายใน (วัน) */
+                                            if (moment(date) < moment(formik.values.notice_date)) {
+                                                toast.error('ไม่สามารถกำหนดวันที่ส่งมอบก่อนวันที่ประกาศผู้ชนะได้!!')
+                                            } else {
+                                                console.log(moment(date).diff(moment(formik.values.notice_date), "day"));
+                                            }
                                         }}
                                     />
+                                    {(formik.errors.deliver_date && formik.touched.deliver_date) && (
+                                        <span className="text-red-500 text-sm">{formik.errors.deliver_date}</span>
+                                    )}
                                 </div>
                             </Col>
                             <Col md={3} className="mt-2">
                                 <label htmlFor="">กำหนดส่งมอบภายใน</label>
-                                <div className="form-control min-h-[34px] text-sm text-center">
+                                <div className="form-control min-h-[34px] text-sm text-center bg-gray-100">
                                     {formik.values.deliver_days} วัน
                                 </div>
+                                {(formik.errors.deliver_days && formik.touched.deliver_days) && (
+                                    <span className="text-red-500 text-sm">{formik.errors.deliver_days}</span>
+                                )}
                             </Col>
                             <Col md={12} className="text-center">
                                 <button type="submit" className="btn btn-outline-primary btn-sm mt-3">
