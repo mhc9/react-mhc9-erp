6658008df0892cfb55ebea3b0ab9bd6a8bf02c40
sanyath007 <sanyath007@gmail.com>
2024-08-16 13:17:34 +0700

    reflactor(BudgetProjectList): add Pagination component and endpoint local state and set condition to dispatch getBudgetProject action in useEffect hook

--- src/views/Budget/Project/List.js
+++ src/views/Budget/Project/List.js
@@ -1,18 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Breadcrumb } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { FaPencilAlt, FaSearch, FaTrash } from 'react-icons/fa'
 import { getBudgetProjects } from '../../../features/slices/budget-project/budgetProjectSlice'
 import Loading from '../../../components/Loading'
+import Pagination from '../../../components/Pagination'
 
 const BudgetProjectList = () => {
     const dispatch = useDispatch();
     const { projects, pager, isLoading } = useSelector(state => state.budgetProject);
+    const [endpoint, setEndpoint] = useState('');
 
     useEffect(() => {
-        dispatch(getBudgetProjects({ url: `/api/budget-projects/search` }))
-    }, []);
+        if (endpoint === '') {
+            dispatch(getBudgetProjects({ url: `/api/budget-projects/search` }));
+        } else {
+            dispatch(getBudgetProjects({ url: `${endpoint}` }));
+        }
+    }, [endpoint]);
 
     const handleDelete = (id) => {
 
@@ -53,8 +59,8 @@ const BudgetProjectList = () => {
                                 <tr>
                                     <td className="text-center">{index+1}</td>
                                     <td>{project.name}</td>
-                                    <td>{project.plan?.name}</td>
-                                    <td className="text-center">{project.year}</td>
+                                    <td>{project.plan?.plan_no} {project.plan?.name}</td>
+                                    <td className="text-center">{project.year && project.year + 543}</td>
                                     <td className="text-center p-1">
                                         <Link to={`/budget-project/${project.id}/detail`} className="btn btn-sm btn-info px-1 mr-1">
                                             <FaSearch />
@@ -71,6 +77,13 @@ const BudgetProjectList = () => {
                         </tbody>
                     </table>
                 </div>
+
+                {pager && (
+                    <Pagination
+                        pager={pager}
+                        onPageClick={(url) => setEndpoint(url)}
+                    />
+                )}
             </div>
         </div>
     )
