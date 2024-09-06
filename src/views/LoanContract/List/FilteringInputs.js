0229797aa668ccee750c8bf7079797c947a8deb4
sanyath007 <sanyath007@gmail.com>
2024-09-06 11:31:02 +0700

    reflactor(LoanContract/List/FilteringInputs): add new input of employees

--- src/views/LoanContract/List/FilteringInputs.js
+++ src/views/LoanContract/List/FilteringInputs.js
@@ -16,19 +16,32 @@ const FilteringInputs = ({ initialFilters, onFilter }) => {
 
     return (
         <div className="border rounded-md py-2 px-3 mb-2">
-            <div className="flex flex-row items-center gap-1">
-                <label htmlFor="">ปีงบ :</label>
-                <div className="w-[20%]">
-                    <DatePicker
-                        format="YYYY"
-                        views={['year']}
-                        value={selectedYear}
-                        onChange={(date) => {
-                            setSelectedYear(date);
-                            setFilters(prev => ({ ...prev, ['year']: moment(date).year() }));
-                        }}
-                        className={classes.muiTextFieldInput}
-                    />
+            <div className="flex flex-row items-center gap-2">
+                <div className="flex flex-row items-center gap-1">
+                    <label htmlFor="">ปีงบ :</label>
+                    <div className="w-[60%]">
+                        <DatePicker
+                            format="YYYY"
+                            views={['year']}
+                            value={selectedYear}
+                            onChange={(date) => {
+                                setSelectedYear(date);
+                                setFilters(prev => ({ ...prev, ['year']: moment(date).year() }));
+                            }}
+                            className={classes.muiTextFieldInput}
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-row items-center gap-1 w-[35%]">
+                    <label htmlFor="">ผู้ยืม :</label>
+                    <div className="w-[70%]">
+                        <select
+                            name=""
+                            className="form-control"
+                        >
+                            <option value="">-- ผู้ยืมทั้งหมด --</option>
+                        </select>
+                    </div>
                 </div>
             </div>
         </div>
