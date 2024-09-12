3c65777c5c4d44e53d5f9c58ac7ce38cdbf89b94
sanyath007 <sanyath007@gmail.com>
2024-09-12 14:01:44 +0700

    reflactor(LoanContractList): reset visible edit and delete buttons according to contract's status

--- src/views/LoanContract/List/index.js
+++ src/views/LoanContract/List/index.js
@@ -118,7 +118,7 @@ const LoanContractList = () => {
                                         <Link to={`/loan-contract/${contract.id}/detail`} className="btn btn-sm btn-info px-1 mr-1">
                                             <FaSearch />
                                         </Link>
-                                        {![3,4].includes(contract.status) && (
+                                        {![2,3,4].includes(contract.status) && (
                                             <>
                                                 <Link to={`/loan-contract/${contract.id}/edit`} className="btn btn-sm btn-warning px-1 mr-1">
                                                     <FaPencilAlt />
