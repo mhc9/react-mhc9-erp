be98fd61f7338c0a0d54ded335c96ca3c6c46876
sanyath007 <sanyath007@gmail.com>
2024-09-12 14:34:58 +0700

    reflactor(Footer): edit text of creator

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer style={{ background: '#0a0b0c' }}>
             <div className="p-4 text-white">
                 <h3>
-                    ©2020 ศูนย์สุขภาพจิตที่ 9 All Rights Reserved | DESIGNED BY 
+                    ©2020 ศูนย์สุขภาพจิตที่ 9 All Rights Reserved | โดย 
                     <a href="https://www.query-studio.com" target="_blank" rel="noreferrer" className="ml-1">
                         นายสัญญา ธรรมวงษ์
                     </a>
