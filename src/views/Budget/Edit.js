f37b4ef27c25733a3ac86d2c4ef86f5ec1de1877
sanyath007 <sanyath007@gmail.com>
2024-09-16 11:08:07 +0700

    reflactor(EditBudget): edit header text

--- src/views/Budget/Edit.js
+++ src/views/Budget/Edit.js
@@ -28,12 +28,12 @@ const EditBudget = () => {
             <Breadcrumb>
                 <Breadcrumb.Item linkAs={Link} linkProps={{ to: '/' }}>หน้าหลัก</Breadcrumb.Item>
                 <Breadcrumb.Item linkAs={Link} linkProps={{ to: '/budget' }}>งบประมาณ</Breadcrumb.Item>
-                <Breadcrumb.Item active>เพิ่มงบประมาณ</Breadcrumb.Item>
+                <Breadcrumb.Item active>แก้ไขงบประมาณ</Breadcrumb.Item>
             </Breadcrumb>
 
             <div className="content">
                 <div className="flex items-center justify-between mb-2">
-                    <h2 className="text-xl">เพิ่มงบประมาณ</h2>
+                    <h2 className="text-xl">แก้ไขงบประมาณ (#{id})</h2>
                 </div>
 
                 <div className="border rounded-md py-5">
