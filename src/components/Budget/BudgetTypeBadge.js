66523e8e2710a7244e1831748ccf09ecca4897e2
sanyath007 <sanyath007@gmail.com>
2024-09-24 11:41:27 +0700

    reflactor(BudgetBadge): reset bg color of each badge and add additional badges

--- src/components/Budget/BudgetTypeBadge.js
+++ src/components/Budget/BudgetTypeBadge.js
@@ -3,9 +3,11 @@ import React from 'react'
 const BudgetTypeBadge = ({ type }) => {
     return (
         <span className="ml-1 text-sm">
-            {type.id === 1 && <span className="badge rounded-pill bg-success">{type?.name}</span>}
-            {type.id === 2 && <span className="badge rounded-pill bg-primary">{type?.name}</span>}
-            {type.id === 3 && <span className="badge rounded-pill bg-danger">{type?.name}</span>}
+            {type.id === 1 && <span className="badge rounded-pill bg-secondary">{type?.name}</span>}
+            {type.id === 2 && <span className="badge rounded-pill bg-danger">{type?.name}</span>}
+            {type.id === 3 && <span className="badge rounded-pill bg-success">{type?.name}</span>}
+            {type.id === 4 && <span className="badge rounded-pill bg-info">{type?.name}</span>}
+            {type.id === 5 && <span className="badge rounded-pill bg-warning">{type?.name}</span>}
         </span>
     );
 };
