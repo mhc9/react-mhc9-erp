d429ae37a828c384ee1b2005584babaaf904ac11
sanyath007 <sanyath007@gmail.com>
2024-09-30 11:16:20 +0700

    reflactor(Login): re-design

--- src/views/Auth/Login.js
+++ src/views/Auth/Login.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Col, Row, Spinner } from 'react-bootstrap'
 import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
-import { useDispatch, useSelector } from 'react-redux'
-import Spinner from 'react-bootstrap/Spinner'
 import { login, resetSuccess } from '../../features/slices/auth/authSlice'
 
 const loginSchema = Yup.object().shape({
@@ -32,10 +32,10 @@ const Login = () => {
     }
 
     return (
-        <div className="bg-gray-200">
-            <div className="container flex flex-col justify-center items-center min-h-[100vh]">
-                <div className="login-box flex flex-col justify-center items-center bg-white border rounded-md py-10 w-1/2 max-[767px]:w-full min-lg:w-4/12 min-[1285px]:w-4/12">
-                    <h2 className="text-xl">ระบบ <b>MHC9</b> ERP</h2>
+        <div className="container flex flex-col justify-center items-center min-h-[100vh]">
+            <div className="login-box bg-white w-[360px] min-h-[360px] rounded-lg px-4 py-4 flex flex-col justify-center items-center">
+                <h1 className="text-3xl font-bold mt-4">ระบบ MHC9 ERP</h1>
+                <div className="w-[100%] my-4">
                     <Formik
                         initialValues={{
                             email: 'sanyath007@gmail.com',
@@ -47,61 +47,66 @@ const Login = () => {
                         {(formik) => {
                             return (
                                 <Form>
-                                    <div>
-                                        <div className="flex max-sm:flex-col gap-2 justify-center items-center my-4">
-                                            <label className="w-3/12 max-sm:w-full max-sm:hidden">ชื่อผู้ใช้ :</label>
-                                            <div className="border px-4 py-2 rounded-full w-3/4 max-sm:w-full">
-                                                <input
-                                                    type="text"
-                                                    name="email"
-                                                    value={formik.values.email}
-                                                    onChange={formik.handleChange}
-                                                    placeholder="Email"
-                                                    className="outline-none"
-                                                />
-                                            </div>
-                                        </div>
+                                    <Row className="mb-2">
+                                    <Col>
+                                        <input
+                                            type="text"
+                                            name="email"
+                                            value={formik.values.email}
+                                            onChange={formik.handleChange}
+                                            className="form-control"
+                                            placeholder="Email"
+                                        />
+                                    </Col>
+                                </Row>
+                                <Row className="mb-4">
+                                    <Col>
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            value={formik.values.password}
+                                            onChange={formik.handleChange}
+                                            className="form-control"
+                                            placeholder="Password"
+                                        />
+                                    </Col>
+                                </Row>
 
-                                        <div className="flex max-sm:flex-col gap-2 justify-center items-center my-4">
-                                            <label className="w-3/12 max-sm:w-full max-sm:hidden">รหัสผ่าน :</label>
-                                            <div className="border px-4 py-2 rounded-full w-3/4 max-sm:w-full">
-                                                <input
-                                                    type="password"
-                                                    name="password"
-                                                    value={formik.values.password}
-                                                    onChange={formik.handleChange}
-                                                    placeholder="Password"
-                                                    className="outline-none"
-                                                />
-                                            </div>
-                                        </div>
+                                <div className="d-grid mb-2">
+                                    <button type="submit" className="btn btn-outline-primary">
+                                        {loading && (
+                                            <Spinner animation="border" role="status" size="sm" style={{ marginRight: '2px' }}>
+                                                <span className="visually-hidden">Loading...</span>
+                                            </Spinner>
+                                        )}
+                                        ล็อกอิน <i className="fas fa-sign-in-alt"></i>
+                                    </button>
+                                </div>
 
-                                        <div className="flex max-sm:flex-col gap-1 justify-center items-center">
-                                            <label className="w-3/12 max-sm:w-full max-sm:hidden"></label>
-                                            <div className="w-3/4 max-sm:w-full flex justify-between items-center">
-                                                <button
-                                                    type="submit"
-                                                    className="bg-green-600 rounded-full px-4 py-2 text-white hover:bg-green-900"
-                                                    disabled={formik.isSubmitting}
-                                                >
-                                                    {loading && (
-                                                        <Spinner animation="border" role="status" size="sm" style={{ marginRight: '2px' }}>
-                                                            <span className="visually-hidden">Loading...</span>
-                                                        </Spinner>
-                                                    )}
-                                                    ล็อกอิน <i className="fas fa-sign-in-alt"></i>
-                                                </button>
-                                                <Link to="/register" className="hover:text-blue-500 mr-1">
-                                                    <i className="fas fa-user-plus"></i> ลงทะเบียน
-                                                </Link>
-                                            </div>
-                                        </div>
+                                <div className="flex flex-row justify-between mb-4 px-2">
+                                    <div className="flex flex-row items-center gap-1">
+                                        <input type="checkbox" name="" /> จำรหัสผ่าน
                                     </div>
+
+                                    <a href="#" className="text-primary">ลืมรหัสผ่าน</a>
+                                </div>
+
+                                <hr />
+
+                                <div className="d-grid mt-4">
+                                    <button type="submit" className="btn btn-outline-secondary">
+                                        <i className="fas fa-user-plus"></i> ลงทะเบียน
+                                    </button>
+                                </div>
                             </Form>
                         )
                     }}
                     </Formik>
                 </div>
+            </div>
+
+            <div className="mt-4 text-gray-600 text-sm">
+                <a href="https:www.mhc9dmh.com">©ศูนย์สุขภาพจิตที่ 9</a> | โดย สัญญา ธรรมวงษ์
             </div>
         </div>
     )
