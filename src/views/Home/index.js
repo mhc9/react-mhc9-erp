32420365f25de290da4cf9e869b54f6ee159efc3
sanyath007 <sanyath007@gmail.com>
2024-10-10 09:14:33 +0700

    reflactor(Home): use useGetSystemInfoQuery hook for fetching system data and set them to cookies

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -2,13 +2,16 @@ import React, { useEffect } from 'react'
 import { useCookies } from 'react-cookie'
 import { FaInfoCircle } from 'react-icons/fa'
 import { Breadcrumb } from 'react-bootstrap'
+import { useGetSystemInfoQuery } from '../../features/services/system/systemApi'
+import Loading from '../../components/Loading'
 
 const Home = () => {
     const [cookies, setCookie] = useCookies(['budgetYear']);
+    const { data: system, isLoading } = useGetSystemInfoQuery();
 
     useEffect(() => {
-        setCookie('budgetYear', 2025);
-    }, []);
+        system && setCookie('budgetYear', system.year);
+    }, [system]);
 
     return (
         <div className="content-wrapper">
@@ -21,10 +24,8 @@ const Home = () => {
             <div className="flex items-center gap-3 border border-green-900 bg-green-300 rounded-md p-4 text-green-800">
                 <FaInfoCircle size={'50px'} />
                 <div className="flex flex-col">
-                    <h1 className="font-bold text-2xl">Welcome to MHC9 ERP System ปีงบประมาณ {cookies.budgetYear}</h1>
-                    {/* <p className="">
-                        {user && user.name}
-                    </p> */}
+                    {isLoading && <Loading />}
+                    <h1 className="font-bold text-2xl">Welcome to MHC9 ERP System {!isLoading && <span>ปีงบประมาณ {cookies.budgetYear + 543}</span>}</h1>
                 </div>
             </div>
 
