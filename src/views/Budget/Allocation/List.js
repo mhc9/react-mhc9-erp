ea01da2a3ffd8e8d4b30c48dd0e310c63906e370
sanyath007 <sanyath007@gmail.com>
2024-11-01 12:01:05 +0700

    reflactor(AllocationList): add fetching allocations data and render it to data table

--- src/views/Budget/Allocation/List.js
+++ src/views/Budget/Allocation/List.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Breadcrumb } from 'react-bootstrap'
+import { getAllocations } from '../../../features/slices/budget-allocation/budgetAllocationSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import Loading from '../../../components/Loading'
+import { currency, toShortTHDate } from '../../../utils'
 
 const AllocationList = () => {
+    const dispatch = useDispatch();
+    const { allocations, pager, isLoading } = useSelector(state => state.budgetAllocation);
+    const [endpoint, setEndpoint] = useState('');
+
+    useEffect(() => {
+        if (endpoint === '') {
+            dispatch(getAllocations({ url: `/api/budget-allocations/search` }));
+        } else {
+            dispatch(getAllocations({ url: `${endpoint}` }));
+        }
+    }, [endpoint]);
+
     return (
         <div className="content-wrapper">
             <Breadcrumb>
@@ -28,18 +44,26 @@ const AllocationList = () => {
                                 <th className="text-center w-[5%]">#</th>
                                 <th className="text-center w-[15%]">อ้างอิง</th>
                                 <th>รายละเอียด</th>
-                                <th className="text-center w-[12%]">ยอดรับโอน</th>
+                                <th className="text-center w-[15%]">ยอดรับโอน</th>
                                 <th className="text-center w-[10%]">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
+                            {isLoading && <tr><td className="text-center" colSpan={5}><Loading /></td></tr>}
+                            {(!isLoading && allocations) && allocations.map((allocation, index) => (
+                                <tr>
+                                    <td className="text-center">{index+pager?.from}</td>
+                                    <td className="text-left">
+                                        <p className="font-thin text-sm"><b>เลขที่</b> {allocation.doc_no}</p>
+                                        <p className="font-thin text-sm"><b>วันที่</b> {toShortTHDate(allocation.doc_date)}</p>
+                                    </td>
+                                    <td>{allocation.description}</td>
+                                    <td className="text-center">{currency.format(allocation.total)}</td>
+                                    <td className="text-center">
+
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
