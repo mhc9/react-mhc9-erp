179a6a08d2dbc138c02b75cb682a8a8bdfa15d0c
sanyath007 <sanyath007@gmail.com>
2025-04-02 14:12:00 +0700

    reflactor(employeeSlice): re-modify CUD and upload operation and add new resetSuccess, resetDeleted and resetUploaded reducers

--- src/features/slices/employee/employeeSlice.js
+++ src/features/slices/employee/employeeSlice.js
@@ -7,6 +7,8 @@ const initialState = {
     pager: null,
     isLoading: false,
     isSuccess: false,
+    isDeleted: false,
+    isUploaded: false,
     error: null
 };
 
@@ -76,6 +78,15 @@ export const employeeSlice = createSlice({
     name: "employee",
     initialState,
     reducers: {
+        resetSuccess: (state) => {
+            state.isSuccess = false;
+        },
+        resetDeleted: (state) => {
+            state.isDeleted = false;
+        },
+        resetUploaded: (state) => {
+            state.isUploaded = false;
+        },
         updateAvatar: (state, { payload }) => {
             state.employee = { ...state.employee, avatar_url: payload };
         }
@@ -112,56 +123,65 @@ export const employeeSlice = createSlice({
             state.error = payload;
         },
         [store.pending]: (state) => {
-            state.isLoading = true;
             state.isSuccess = false;
             state.error = null;
         },
         [store.fulfilled]: (state, { payload }) => {
-            console.log(payload);
-            state.isLoading = false;
-            state.isSuccess = true;
+            const { status, message, employee } = payload;
+
+            if (status === 1) {
+                state.isSuccess = true;
+            } else {
+                state.error = { message };
+            }
         },
         [store.rejected]: (state, { payload }) => {
-            console.log(payload);
-            state.isLoading = false;
             state.error = payload;
         },
         [update.pending]: (state) => {
-            state.isLoading = true;
             state.isSuccess = false;
             state.error = null;
         },
         [update.fulfilled]: (state, { payload }) => {
-            console.log(payload);
-            state.isLoading = false;
-            state.isSuccess = true;
+            const { status, message, employee } = payload;
+
+            if (status === 1) {
+                state.isSuccess = true;
+            } else {
+                state.error = { message };
+            }
         },
         [update.rejected]: (state, { payload }) => {
-            console.log(payload);
-            state.isLoading = false;
             state.error = payload;
         },
         [destroy.pending]: (state) => {
-            state.isLoading = true;
-            state.isSuccess = false;
+            state.isDeleted = false;
             state.error = null;
         },
         [destroy.fulfilled]: (state, { payload }) => {
-            console.log(payload);
-            state.isLoading = false;
-            state.isSuccess = true;
+            const { status, message } = payload;
+
+            if (status === 1) {
+                state.isDeleted = true;
+            } else {
+                state.error = { message };
+            }
         },
         [destroy.rejected]: (state, { payload }) => {
-            console.log(payload);
-            state.isLoading = false;
             state.error = payload;
         },
         [upload.pending]: (state) => {
-            state.isSuccess = false;
+            state.isUploaded = false;
             state.error = null;
         },
         [upload.fulfilled]: (state, { payload }) => {
-            state.isSuccess = true;
+            const { status, message } = payload;
+
+            if (status === 1) {
+                state.isUploaded = true;
+            } else {
+                state.error = { message };
+            }
         },
         [upload.rejected]: (state, { payload }) => {
             state.error = payload;
@@ -171,4 +191,9 @@ export const employeeSlice = createSlice({
 
 export default employeeSlice.reducer;
 
-export const { updateAvatar } = employeeSlice.actions;
+export const {
+    resetDeleted,
+    resetSuccess,
+    resetUploaded,
+    updateAvatar
+} = employeeSlice.actions;
