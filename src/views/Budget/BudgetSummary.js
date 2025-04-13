ddf33a83708f7d88d3e3b4627eef6817e08bd517
sanyath007 <sanyath007@gmail.com>
2025-04-13 13:19:14 +0700

    reflactor(BudgetSummary): add new external var and pass it to setSummary local state and re-render summary by iterating its keys

--- src/views/Budget/BudgetSummary.js
+++ src/views/Budget/BudgetSummary.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { FaRegUser, FaRandom } from 'react-icons/fa'
+import { FaLandmark, FaRegUser, FaRandom } from 'react-icons/fa'
 import { getAllBudgetsOfYear } from '../../features/slices/budget/budgetSlice'
 import { currency } from '../../utils/index'
 
@@ -18,39 +18,33 @@ const BudgetSummary = ({ year }) => {
         if (!isLoading && (budgets && budgets.length > 0)) {
             const personnel = budgets.reduce((sum, budget) => sum += budget.budget_type_id === 1 ? parseFloat(budget.total) : 0, 0);
             const operation = budgets.reduce((sum, budget) => sum += budget.budget_type_id === 2 ? parseFloat(budget.total) : 0, 0);
+            const external = budgets.reduce((sum, budget) => sum += budget.budget_type_id === 6 ? parseFloat(budget.total) : 0, 0);
 
-            setSummary({ personnel, operation })
+            setSummary({ personnel, operation, external })
         }
     }, [budgets]);
-
+    
     return (
         <div className="border rounded-md py-3 px-5 mb-2 bg-[#D6EADF]">
-            <div className="flex flex-row mb-3">
-                <h3 className="w-[50%] text-xl font-semibold flex items-center gap-1"><FaRegUser /> งบบุคลากร</h3>
-                <div className="w-[50%] flex flex-row justify-around">
-                    <div className="flex flex-col justify-center items-center gap-1 w-[20%]">
-                        <h5 className="font-light">เป้าหมาย</h5>
-                        <span className="font-bold text-lg">0</span>
-                    </div>
-                    <div className="flex flex-col justify-center items-center gap-1 w-[20%]">
-                        <h5 className="font-light">ได้รับ</h5>
-                        <span className="font-bold text-lg">{currency.format(summary?.personnel)}</span>
-                    </div>
-                </div>
-            </div>
-            <div className="flex flex-row">
-                <h3 className="w-[50%] text-xl font-semibold flex items-center gap-1"><FaRandom /> งบดำเนินงาน</h3>
-                <div className="w-[50%] flex flex-row justify-around">
-                    <div className="flex flex-col justify-center items-center gap-1 w-[20%]">
-                        <h5 className="font-light">เป้าหมาย</h5>
-                        <span className="font-bold text-lg">0</span>
-                    </div>
-                    <div className="flex flex-col justify-center items-center gap-1 w-[20%]">
-                        <h5 className="font-light">ได้รับ</h5>
-                        <span className="font-bold text-lg">{currency.format(summary?.operation)}</span>
+            {summary && Object.keys(summary).map((sum, index) => (
+                <div className="flex flex-row mb-3">
+                    <h3 className="w-[50%] text-xl font-semibold flex items-center gap-1">
+                        {sum === 'personnel' && <><FaRegUser /> งบบุคลากร</>}
+                        {sum === 'operation' && <><FaRandom /> งบดำเนินงาน</>}
+                        {sum === 'external' && <><FaLandmark /> เงินนอกงบประมาณ</>}
+                    </h3>
+                    <div className="w-[50%] flex flex-row justify-around">
+                        <div className="flex flex-col justify-center items-center gap-1 w-[20%]">
+                            <h5 className="font-light">เป้าหมาย</h5>
+                            <span className="font-bold text-lg">0</span>
+                        </div>
+                        <div className="flex flex-col justify-center items-center gap-1 w-[20%]">
+                            <h5 className="font-light">ได้รับ</h5>
+                            <span className="font-bold text-lg">{currency.format(summary[sum])}</span>
+                        </div>
                     </div>
                 </div>
-            </div>
+            ))}
         </div>
     )
 }
